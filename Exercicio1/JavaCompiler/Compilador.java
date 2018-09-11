7d9a821879c05c32685a8ea137487fcb0aead157
diegopoleto <diegopoleto10@gmail.com>
2018-09-11 14:42:19 -0300

    nada

---
+++ Exercicio1/JavaCompiler/Compilador.java
@@ -0,0 +1,39 @@
+class Compilador{
+
+	public static void main(String[]args)
+	{	
+		ArvoreSintatica arv=null;
+	
+		try{
+
+			AnaliseLexica al = new AnaliseLexica(args[0]);
+			Parser as = new Parser(al);
+		
+			arv = as.parseProg();
+		
+			
+			CodeGen backend = new CodeGen();
+			String codigo = backend.geraCodigo(arv);
+			System.out.println(codigo);
+			System.out.println();
+
+			al = new AnaliseLexica(args[0]);
+			as = new Parser(al);
+		
+			arv = as.parseProg();
+
+			CodeInterpreter interpreter = new CodeInterpreter();
+			interpreter.interpret(arv);
+
+			System.out.println("Resultado: " + interpreter.getResult());
+
+		}catch(Exception e)
+		{			
+			e.printStackTrace();
+			System.out.println("Erro de compilação:\n" + e);
+		}
+
+
+
+	}
+}
---
+++ Trabalho1/MaquinaDePilha.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.Stack;
+
+class MaquinaDePilha {
+
+	public static void main(String[] args) {
+		BufferedReader arquivo;
+		try {
+		 	arquivo = new BufferedReader(new FileReader(args[0]));
+
+		 	Stack<Integer> pilha = new Stack<>();
+		 	int op1, op2;
+
+		 	String linha = arquivo.readLine();
+		 	while(linha != null){
+
+		 		String[] splitted = linha.split(" ");
+		 		String first = splitted[0];
+		 		if(first.equalsIgnoreCase("PUSH")){
+		 			pilha.push(new Integer(splitted[1]));
+		 		}
+		 		else if (first.equalsIgnoreCase("SUM")) {
+		 			op2 = pilha.pop().intValue();
+		 			op1 = pilha.pop().intValue();
+		 			pilha.push(new Integer(op1 + op2));
+		 		}
+		 		else if (first.equalsIgnoreCase("SUBT")) {
+		 			op2 = pilha.pop().intValue();
+		 			op1 = pilha.pop().intValue();
+		 			pilha.push(new Integer(op1 - op2));
+		 		}
+		 		else if (first.equalsIgnoreCase("MULT")) {
+		 			op2 = pilha.pop().intValue();
+		 			op1 = pilha.pop().intValue();
+		 			pilha.push(new Integer(op1 * op2));
+		 		}
+		 		else if (first.equalsIgnoreCase("DIV")) {
+		 			op2 = pilha.pop().intValue();
+		 			op1 = pilha.pop().intValue();
+		 			pilha.push(new Integer(op1 / op2));
+		 		}
+		 		else if (first.equalsIgnoreCase("PRINT")) {
+		 			System.out.println(pilha.peek().intValue());
+		 		}
+		 		else {
+		 			System.out.println("erro: '" + linha + "'");
+		 		}
+
+		 		linha = arquivo.readLine();
+		 	}
+		}
+		catch (Exception e) {
+			System.out.println("Algo de errado não está certo: ");
+			e.printStackTrace();
+		}		
+	}
+}
